3d194d5d2896fa04e832aae265d30966fc430867
BuildTools <unconfigured@null.spigotmc.org>
2020-05-18 12:19:32 +0200

    mostrar dificultad menu

--- src/paginas/Home.js
+++ src/paginas/Home.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React,{Component} from "react";
 import { Button } from "../componentes/ButtonRedirect";
 
-export const Home = () => (
+export class Home extends Component{
+  render(){
+    return (
   <div className="container bg-main py-5">
     <div className="row justify-content-center">
       <div className="col-4 text-center">
@@ -10,8 +12,9 @@ export const Home = () => (
     </div>
     <div className="row justify-content-center">
       <div className="col-4 text-center">
-        <Button to="/dificultad" texto="Elegir dificultad" margen="mt-4 mb-5" />
+        <Button to="/dificultad" texto={`Elegir dificultad: ${this.props.dificultad}`} margen="mt-4 mb-5" />
       </div>
     </div>
   </div>
 );
+    }}
